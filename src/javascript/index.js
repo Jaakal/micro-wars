b7bfe6ea684376af3de3a8cf6cc3158504e2b3fd
Jaakal <jaak.kivinukk@gmail.com>
2020-08-06 19:32:45 +0300

    Shooting in all directions

--- src/javascript/index.js
+++
@@ -1,6 +0,0 @@
-import '../css/reset.css';
-import '../css/style.scss';
-
-import Game from './objects/game';
-
-window.game = new Game();
--- src/javascript/objects/enemy-laser.js
+++
@@ -1,8 +0,0 @@
-import Entity from './entity';
-
-export default class EnemyLaser extends Entity {
-  constructor(scene, x, y) {
-    super(scene, x, y, 'enemyLaser');
-    this.body.velocity.y = 200;
-  }
-}
